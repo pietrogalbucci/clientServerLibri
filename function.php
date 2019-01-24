983c958553a6704054f1bd9c24dcf6642e597e29
pietrogalbucci <pietro.galbucci@gmail.com>
2019-01-24 09:43:05 +0100

    Parte aggiornata

--- function.php
+++ function.php
@@ -1,22 +1,9 @@
 <?php
  function get_price($find){
-	/* $books=array(
-	 "java"=>299,
-	 "c"=>348,
-	 "php"=>267
-	 );*/
+
 	$tmp = "book.json";
 	$str = file_get_contents($tmp);
 	$books = json_decode($str, true); 
-	// echo '<pre>' . print_r($books, true) . '</pre>';
-	/* foreach($books as $book=>$price)
-	 {
-		 if($book==$find)
-		 {
-			 return $price;
-			 break;
-		 }
-	 }*/
 	 
 	 foreach($books['book'] as $book)
 	 {
@@ -26,6 +13,8 @@
 			 break;
 		 }
 	 }
+	 
+	 
  }
 
 ?>
